857efd4d82d69a7f5594a356435b65449a54f9b6
jacquelinewattles <jacqueline.wattles@gmail.com>
2015-04-02 16:52:16 -0400

    update

--- leafletmap/Exercise 2/js/main.js
+++ leafletmap/Exercise 2/js/main.js
@@ -1,4 +1,11 @@
 (function(){
+
+	var map = L.map('map').setView([39.2500,-97.743061], 4);
+
+	L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png', {
+	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	    maxZoom: 18
+	}).addTo(map);
 	
 
 	var locations = {
@@ -28,13 +35,6 @@
 		}
 
 	};
-
-	var map = L.map('map').setView([39.2500,-97.743061], 4);
-
-	L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png', {
-	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	    maxZoom: 18
-	}).addTo(map);
 
 	$('.option').on('click', function(){
 		var id = $(this).attr('data-which');
